249aa1ea7701922a68ba1bd982dd071beeb5dd73
bearhox <75946315+bearhox@users.noreply.github.com>
2025-10-21 14:29:34 -0400

    Create historic_station.py

---
+++ 2025-10-21/historic_station.py
@@ -0,0 +1,4 @@
+import pandas
+
+historic_station_met = pandas.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-10-21/historic_station_met.csv')
+station_meta = pandas.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-10-21/station_meta.csv')
