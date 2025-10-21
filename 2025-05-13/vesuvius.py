5ced2df181c2bc3e1a0789d19d961fb8ef81d89d
bearhox <75946315+bearhox@users.noreply.github.com>
2025-10-21 14:18:19 -0400

    Update vesuvius.py

--- 2025-05-13/vesuvius.py
+++ 2025-05-13/vesuvius.py
@@ -1,16 +1,9 @@
-
-# Using Python
-# Option 1: pydytuesday python library
-## pip install pydytuesday
 import pandas
-import pydytuesday
 
-# Download files from the week, which you can then read in locally
-pydytuesday.get_date('2025-05-13')
-
-# Option 2: Read directly from GitHub and assign to an object
+# read directly from GitHub and assign to an object
 vesuvius = pandas.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-13/vesuvius.csv')
 
+#convert dates and times
 vesuvius['time'] = pandas.to_datetime(vesuvius['time'])
 vesuvius['year'] = vesuvius['time'].dt.year
 vesuvius['month'] = vesuvius['time'].dt.month
@@ -22,8 +15,7 @@ vesuvius['weekday'] = vesuvius['time'].dt.day_name()
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-
+# plot visual of hourly count of earthquakes
 plt.figure(figsize=(10, 5))
 sns.countplot(data=vesuvius, x="hour")
 plt.title("Earthquakes near Vesuvius by Hour of Day")
@@ -31,10 +23,10 @@ plt.xlabel("Hour")
 plt.ylabel("Count")
 plt.show()
 
-
-""" plt.figure(figsize=(8, 6))
+# plot visual of depth and duration
+plt.figure(figsize=(8, 6))
 sns.scatterplot(data=vesuvius, x="depth_km", y="duration_magnitude_md", alpha=0.6)
 plt.title("Earthquake Depth vs. Magnitude")
 plt.xlabel("Depth (km)")
 plt.ylabel("Duration Magnitude (Md)")
-plt.show() """
+plt.show() 
