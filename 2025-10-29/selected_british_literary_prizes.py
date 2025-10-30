119124d902a90ee0fc507c3c5528f43625a8f993
bearhox <75946315+bearhox@users.noreply.github.com>
2025-10-30 15:23:48 -0400

    Update selected_british_literary_prizes.py

--- 2025-10-29/selected_british_literary_prizes.py
+++ 2025-10-29/selected_british_literary_prizes.py
@@ -1,1 +1,8 @@
+# View all prizes that fall under British Literary Prizes
+import pandas
+prizes = pandas.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-10-28/prizes.csv')
+prize_name = 'prize_name'
+distinct_values = sorted(prizes[prize_name].drop_duplicates())
 
+for prize in distinct_values:
+    print(prize)
