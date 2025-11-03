68442f0c7e76700c777629a061c9e023d044889a
bearhox <75946315+bearhox@users.noreply.github.com>
2025-11-03 13:40:37 -0500

    Update selected_british_literary_prizes.py

--- 2025-10-29/selected_british_literary_prizes.py
+++ 2025-10-29/selected_british_literary_prizes.py
@@ -1,8 +1,15 @@
-# View all prizes that fall under British Literary Prizes
 import pandas
 prizes = pandas.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-10-28/prizes.csv')
+
+# View header
+print(prizes.columns.tolist())
+
+# View distinct prizes in the data
 prize_name = 'prize_name'
 distinct_values = sorted(prizes[prize_name].drop_duplicates())
+print(distinct_values)
 
-for prize in distinct_values:
-    print(prize)
+# View distinct genres in the data
+prize_genre = 'prize_genre'
+distinct_values_genre = sorted(prizes[prize_genre].drop_duplicates())
+print(distinct_values_genre)
